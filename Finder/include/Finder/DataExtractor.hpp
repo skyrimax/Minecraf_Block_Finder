204e8e027961defb2c5161d58b61c4451c7657db
Maxime Royal <MaxRoyal123@hotmail.com>
2022-11-21 03:44:28 -0500

    chore - clean unused/deprecated functions from DatExtractor

--- Finder/include/Finder/DataExtractor.hpp
+++ Finder/include/Finder/DataExtractor.hpp
@@ -17,12 +17,6 @@ public:
 	static std::vector<std::vector<std::vector<int>>> extractChunk(std::filesystem::path pathToRegion,
 		int chunkX, int chunkZ);
 
-	static std::vector<chunkParams> extractChunksParamsFromWorld(std::filesystem::path pathToDim,
-		int regionX, int regionZ);
-
-	static std::vector<int> extractSectionDataFromWorld(std::filesystem::path pathToDim,
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
-
 	static int maxSectionY() { return m_maxSectionY; }
 	static int nbChunksInRegionX() { return m_nbChunksInRegionX; }
 	static int nbChunksInRegionZ() { return m_nbChunksInRegionZ; }
@@ -32,20 +26,6 @@ private:
 	static void loadChunkDataFromWorld(std::filesystem::path pathToRegion, size_t chunkX, size_t chunkZ);
 	static std::vector<std::vector<std::vector<int>>> parseChunkDataFromWorld(
 		std::filesystem::path pathToRegion, size_t chunkX, size_t chunkZ);
-
-	static void loadSectionDataFromWorld(std::filesystem::path pathToDim,
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
-	static std::vector<int> parseSectionDataFromWorld(
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
-
-	static void loadSectionYDataFromWorld(std::filesystem::path pathToDim,
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
-
-	static bool testIfSectionExist(std::filesystem::path pathToDim,
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
-
-	static bool testIfChunkExist(std::filesystem::path pathToDim,
-		int regionX, int regionZ, size_t chunkX, size_t chunkZ);
 
 	static std::string sectionDataFileName(int regionX, int regionZ, size_t chunkX, size_t chunkZ, size_t sectionY);
 	static std::string chunkDataFilename(std::filesystem::path regionFile, size_t chunkX, size_t chunkZ);
