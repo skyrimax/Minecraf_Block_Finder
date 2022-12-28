271a94e00ed465a258ff073742e658329b235bbd
Maxime Royal <MaxRoyal123@hotmail.com>
2022-12-28 01:51:08 -0500

    freat - declared the 6 comparison operators

--- Finder/Coordinate/include/Coordinate/Coordinate.hpp
+++ Finder/Coordinate/include/Coordinate/Coordinate.hpp
@@ -21,6 +21,33 @@ struct Coordinate
 	Coordinate& operator= (const Coordinate&) = default;
 	Coordinate& operator= (Coordinate&&) = default;
 
+	// Comparisson operators
+
+	/**
+	 * @brief compares 2 coordinates by witchever one is closer to origin \
+	 * 			if the two are at the same distance, compares x, y and z in that order
+	 * 
+	 * @param lhs 
+	 * @param rhs 
+	 * @return true if the lhs coordinate is closer to origin or it x, y, z is smaller (first one to differ)
+	 * @return false if the rhs coordinate is closer to origin or it x, y, z is smaller (first one to differ)
+	 */
+	friend bool operator< (const Coordinate& lhs, const Coordinate& rhs);
+	friend bool operator> (const Coordinate& lhs, const Coordinate& rhs);
+	friend bool operator<= (const Coordinate& lhs, const Coordinate& rhs);
+	friend bool operator<= (const Coordinate& lhs, const Coordinate& rhs);
+
+	/**
+	 * @brief 
+	 * 
+	 * @param lhs 
+	 * @param rhs 
+	 * @return true 
+	 * @return false 
+	 */
+	friend bool operator== (const Coordinate& lhs, const Coordinate& rhs);
+	friend bool operator!= (const Coordinate& lhs, const Coordinate& rhs);
+
 	int x;
 	int y;
 	int z;
