e2051502dc3d2a5f3f1a9a3882c4085cd745cdf9
Maxime Royal <MaxRoyal123@hotmail.com>
2022-12-28 01:10:42 -0500

    feat - add a write coordinate to output stream function
    chore - included ostream to the Coordinate.hpp file

--- Finder/Coordinate/include/Coordinate/Coordinate.hpp
+++ Finder/Coordinate/include/Coordinate/Coordinate.hpp
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <ostream>
+
 struct Coordinate
 {
 	// Constructors
@@ -22,3 +25,14 @@ struct Coordinate
 	int y;
 	int z;
 };
+
+	// Stream operators
+
+	/**
+	 * @brief Writes a coordinate to an output stream in the format (x,y,z)
+	 * 
+	 * @param os Output stream to witch the Coordinate will be writen
+	 * @param coordinate Coordinate object to be writen
+	 * @return the output stream
+	 */
+	std::ostream& operator<<(std::ostream& os, const Coordinate& coordinate);
--- Finder/Coordinate/src/Coordinate.cpp
+++ Finder/Coordinate/src/Coordinate.cpp
@@ -1,1 +1,11 @@
 #include "Coordinate.hpp"
+
+// Stream operators
+
+// Writes a coordinate to an output stream in the format (x,y,z)
+std::ostream& operator<<(std::ostream& os, const Coordinate& coordinate)
+{
+    os << "(" << coordinate.x << "," << coordinate.y << "," << coordinate.z << ")";
+
+    return os;
+}
